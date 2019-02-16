38a11708695d8f5bd401574c5f1ec0014e1eafa7
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-02-16 09:14:22 -0200

    Rename & update run.sh

--- Run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir HDD
-cd HDD
-wget https://www.hdsentinel.com/hdslin/hdsentinel-017-x64.gz
-apt-get install gzip
-gzip -d hdsentinel-017-x64.gz
-chmod 777 hdsentinel-017-x64
-./hdsentinel-017-x64
-exit
---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir HDD
+cd HDD
+wget https://www.hdsentinel.com/hdslin/hdsentinel-017.gz
+gzip -d hdsentinel-017.gz
+chmod 777 hdsentinel-017.gz
+./hdsentinel-017
+exit
