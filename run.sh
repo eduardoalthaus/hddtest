63f2b56844468b35f83cfcc9546ff61685a32dd3
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-04-18 16:26:40 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 mkdir HDD
 cd HDD
-wget https://www.hdsentinel.com/hdslin/hdsentinel-017.gz
+cInfo=`arch`
+if [ $cInfo == "x86_64" ];
+  then
+    wget https://www.hdsentinel.com/hdslin/hdsentinel-017-x64.gz
+gzip -d hdsentinel-017-x64.gz
+chmod +x hdsentinel-017-x64
+./hdsentinel-017x-64
+
+  else
+    wget https://www.hdsentinel.com/hdslin/hdsentinel-017.gz
 gzip -d hdsentinel-017.gz
 chmod +x hdsentinel-017
 ./hdsentinel-017
-exit
+fi
